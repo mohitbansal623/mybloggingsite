2f25499594abe47c2eb84eb121387d482732e6b7
mohitbansal623 <mohit.bansal623@gmail.com>
2018-10-14 14:09:00 +0530

    mysite data

--- sites/all/themes/sub/my_blog_theme/js/home.js
+++ sites/all/themes/sub/my_blog_theme/js/home.js
@@ -1,11 +1,7 @@
-/*
- * @file implementation of Radio Button design
- */
-(function($) {
-   Drupal.behaviors.homejs = {
+(function($, window, document, undefined) {
+  Drupal.behaviors.home_page = {
     attach: function (context, settings) {
-  $('a').click({
-     $('a').attr("target","_blank");
-  });
-  }
-};})(jQuery);
+particlesJS("particle", {"particles":{"number":{"value":80,"density":{"enable":true,"value_area":800}},"color":{"value":"#9bc2cf"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":0.5,"random":false,"anim":{"enable":false,"speed":1,"opacity_min":0.1,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":40,"size_min":0.1,"sync":false}},"line_linked":{"enable":true,"distance":150,"color":"#81D5CE","opacity":0.4,"width":1},"move":{"enable":true,"speed":6,"direction":"none","random":false,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":1200}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"repulse"},"onclick":{"enable":true,"mode":"push"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":400,"size":40,"duration":2,"opacity":8,"speed":3},"repulse":{"distance":200,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
+ }
+  };
+ })(jQuery, window, document);
--- sites/all/themes/sub/my_blog_theme/template.php
+++ sites/all/themes/sub/my_blog_theme/template.php
@@ -1,78 +1,40 @@
 <?php
 /**
  * @file
- * The primary PHP file for the Drupal Bootstrap base theme.
- *
- * This file should only contain light helper functions and point to stubs in
- * other files containing more complex functions.
- *
- * The stubs should point to files within the `./includes` folder named after
- * the function itself minus the theme prefix. If the stub contains a group of
- * functions, then please organize them so they are related in some way and name
- * the file appropriately to at least hint at what it contains.
- *
- * All [pre]process functions, theme functions and template files lives inside
- * the `./templates` folder. This is a highly automated and complex system
- * designed to only load the necessary files when a given theme hook is invoked.
- *
- * Visit this project's official documentation site, http://drupal-bootstrap.org
- * or the markdown files inside the `./docs` folder.
- *
- * @see _bootstrap_theme()
+ * The primary PHP file for this theme.
  */
 
 /**
- * Include common functions used through out theme.
+ * Implements template_preprocess_node
  */
-// include_once dirname(__FILE__) . '/includes/common.inc';
-
-/**
- * Include any deprecated functions.
- */
-// bootstrap_include('bootstrap', 'includes/deprecated.inc');
-
-/**
- * Implements hook_theme().
- *
- * Register theme hook implementations.
- *
- * The implementations declared by this hook have two purposes: either they
- * specify how a particular render array is to be rendered as HTML (this is
- * usually the case if the theme function is assigned to the render array's
- * #theme property), or they return the HTML that should be returned by an
- * invocation of theme().
- *
- * @see _bootstrap_theme()
- */
-function my_blog_theme_theme(&$existing, $type, $theme, $path) {
-  // dpm($theme);
-  // bootstrap_include($theme, 'includes/registry.inc');
-  bootstrap_include('bootstrap', 'includes/registry.inc');
-
-  return _bootstrap_theme($existing, $type, $theme, $path);
-}
-
 function my_blog_theme_preprocess_node(&$variables) {
 
   if(arg(0) == 'node' && is_numeric(arg(1)) && arg(1) > 0) {
     $nid = arg(1);
     $node = node_load($nid);
     if ($node->type == 'create_blog') {
-      drupal_add_css('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', 'external');
       $output = '<div class="icons"><div class="social-media-icons"><p class="share-text">Share On:</p>';
       $theme_path = path_to_theme();
       drupal_add_css($theme_path . '/stylesheets/node-blogs.css', 'file');
+
+      GLOBAL $base_url;
+      GLOBAL $base_path;
+      $current_url = $base_url . $base_path . drupal_get_path_alias(current_path());
+
       // facebook
-      $output .= '<a class="fa fa-facebook" href = "http://www.facebook.com/sharer.php?u=MYBLOG&hashtag=%23MyBlog"></a>';
+      $output .= '<a class="fa fa-facebook" href = "http://www.facebook.com/sharer.php?u=' . $current_url . '&hashtag=%23' . $node->title . '"></a>';
 
       // Twitter.
-      $output .= '<a class="fa fa-twitter" href=https://twitter.com/intent/tweet?text=MyBlog&hashtags=MyBlog></a></div></div> ';
+      $output .= '<a class="fa fa-twitter" href=https://twitter.com/intent/tweet?&url=' . $current_url . '&hashtags=' . $node->title . '></a></div></div> ';
 
       $variables['content']['field_social_media_icons'][0]['#markup'] = $output;
     }
   }
 }
 
+/**
+ * Implements template_preprocess_views_view
+ */
 function my_blog_theme_preprocess_views_view(&$vars) {
   if (arg(0) == 'user' && arg(1) > 0) {
     $userid = arg(1);
@@ -84,25 +46,46 @@ function my_blog_theme_preprocess_views_view(&$vars) {
   $path = current_path();
   $theme_path = path_to_theme();
   switch ($path) {
-    case 'tech-section':
-    case 'nontech-section':
+    case 'techsection':
+    case 'nontechsection':
       drupal_add_css($theme_path . '/stylesheets/education-section.css', 'file');
     break;
-    case 'my-profile':
+    case 'myprofile':
       drupal_add_css($theme_path . '/stylesheets/my-profile.css', 'file');
+      drupal_add_css($theme_path . '/stylesheets/node-blogs.css', 'file');
     break;
     case 'user/' . $userid :
       drupal_add_css($theme_path . '/stylesheets/user-dashboard.css', 'file');
     break;
-    case 'my-projects':
+    case 'myprojects':
       drupal_add_css($theme_path . '/stylesheets/my-projects.css', 'file');
+    break;
+    case 'blogs':
+      drupal_add_css($theme_path . '/stylesheets/blogs.css', 'file');
     break;
   }
 }
 
+/**
+ * Implements template_preprocess_page
+ */
 function my_blog_theme_preprocess_page(&$variables) {
+  drupal_add_css('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', 'external');
   $theme_path = path_to_theme();
   drupal_add_css($theme_path . '/stylesheets/basic_layout.css', 'file');
+  $path = current_path();
+  switch ($path) {
+    case 'node':
+    case 'myprojects':
+    case 'myprofile':
+    case 'user/login':
+    case 'user/register':
+    case 'user/password':
+      drupal_add_js($theme_path . '/js/particles.js', array('scope' => 'footer'));
+      drupal_add_js($theme_path . '/js/home.js', array('scope' => 'footer'));
+    break;
+  }
+
   if (user_is_logged_in()) {
     $variables['page']['footer']['webform_client-block-29']['#block']->subject = "Contact Mohit Bansal";
   }
@@ -110,6 +93,21 @@ function my_blog_theme_preprocess_page(&$variables) {
     $variables['page']['footer']['webform_client-block-29']['#block']->subject = "Contact Me";
   }
 }
+
+/**
+ * Implements template_preprocess_html
+ */
+function my_blog_theme_preprocess_html(&$variables) {
+  $variables['body_attributes_array']['id'] = 'particles-js';
+}
+
+/**
+ * Implements hook_page_build()
+ */
+// function my_blog_theme_page_build(&$page) {
+//   kpr($page);
+//   watchdog('page_buid', print_r($page, TRUE));
+// }
 
 // function my_blog_theme_node_insert($node) {
 //   watchdog('template', print_r(user_is_logged_in(), TRUE));
@@ -127,23 +125,3 @@ function my_blog_theme_preprocess_page(&$variables) {
   // }
 // }
 
-
-
-
-/**
- * Clear any previously set element_info() static cache.
- *
- * If element_info() was invoked before the theme was fully initialized, this
- * can cause the theme's alter hook to not be invoked.
- *
- * @see https://www.drupal.org/node/2351731
- */
-drupal_static_reset('element_info');
-
-/**
- * Declare various hook_*_alter() hooks.
- *
- * hook_*_alter() implementations must live (via include) inside this file so
- * they are properly detected when drupal_alter() is invoked.
- */
-bootstrap_include('bootstrap', 'includes/alter.inc');
